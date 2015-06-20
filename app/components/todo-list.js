45c16d3a3ef15227bda510b0adf134f1f9ab4498
tsara27 <tsara.tf@gmail.com>
2015-06-20 12:09:14 +0700

    add dynamic count

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -6,5 +6,13 @@ export default Ember.Component.extend({
       this.set('newTitle', '');
       this.sendAction('createTodo', newTitle);
     }
-  }
+  },
+  remaining: Ember.computed('model.@each.isCompleted', function() {
+    var model = this.get('model');
+    return model.filterBy('isCompleted', false).get('length');
+  }),
+  inflection: Ember.computed('remaining', function() {
+    var remaining = this.get('remaining');
+    return (remaining === 1) ? 'item' : 'items';
+  }),
 });
